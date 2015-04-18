1043d31b5891f27674d9e62a01b0742f938c8b44
vrunoa <vruno.alassia@gmail.com>
2015-04-18 10:10:44 -0300

    add params to intent

--- public/js/controller.js
+++ public/js/controller.js
@@ -1,6 +1,19 @@
 angular.module('SchemeApp', [])
 
+.filter('stringify', function() {
+  return function(params) {
+    var strParams = [];
+    for(k in params) {
+      if(params[k].name == "" || params[k].name == undefined) continue;
+      strParams.push(params[k].name+"="+params[k].value);
+    }
+    return strParams.join('&');
+  }
+})
+
 .controller('SchemeController', ['$scope', function($scope) {
+
+  $q = jQuery.noConflict();
 
   $scope.platforms = ['android', 'ios'];
   $scope.platform  = 'android';
@@ -16,8 +29,40 @@ angular.module('SchemeApp', [])
 
   $scope.params = [];
 
+  $scope.addParam = function() {
+    $scope.params.push({name:"", value:""});
+    console.table($scope.params);
+  }
+
+  $scope.removeParam = function(i) {
+    $scope.params.splice(i,1);
+  }
+
   $scope.test = function() {
-    alert("TODO");
+    
+  }
+
+  $scope.error = "";
+  $scope.validate = function() {
+    
+    if($scope.platform == 'ios') {
+      return;
+    }
+
+    if($scope.scheme == "") {
+      $q("#scheme").popover('show');
+      return; 
+    }
+
+    if($scope.host == "") {
+      $q("#host").popover('show');
+      return;
+    }
+
+    if($scope.package == "") {
+      $q("#package").popover('show');
+      return;
+    }
   }
 
 }])
