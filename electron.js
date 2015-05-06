9a0b797d04c6f7ddbcb9eb499fa165f6864c0b27
vrunoa <vruno.alassia@gmail.com>
2015-05-06 11:05:38 -0300

    zoster is now an electronjs app

---
+++ electron.js
@@ -0,0 +1,40 @@
+var app = require('app');
+var BrowserWindow = require('browser-window');
+
+var mainWindow = null;
+
+app.on('window-all-closed', function() {
+  if (process.platform != 'darwin')
+    app.quit();
+});
+
+app.on('ready', function() {
+  mainWindow = new BrowserWindow({width: 800, height: 600});
+
+  var zoster = require('./node/');
+  var port = process.env.PORT || "5000"; 
+  var url  = [
+    'http://',
+    '0.0.0.0:',
+    port
+  ].join("");
+  
+  zoster(port).listen(port, '0.0.0.0', function(err){
+      
+     if(err) {
+      console.log(err);
+      mainWindow = null;
+      app.quit();
+      return;
+    }
+    
+    mainWindow.loadUrl(url);
+    mainWindow.on('closed', function() {
+      mainWindow = null;
+    });
+
+    mainWindow.openDevTools({detach:true});
+
+  });
+  
+});
