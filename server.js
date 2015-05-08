619b00e81cf99b3a5656381dedcd58e57d83af27
vrunoa <vruno.alassia@gmail.com>
2015-05-08 09:26:56 -0300

    export server script as a module

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
-var zoster = require('./node/');
-var port = process.env.PORT || "5000"; 
-var url  = ['http://','0.0.0.0:',port].join("");
+'use strict'
+module.exports = function(open, cb) {
+  var zoster = require('./node/');
+  var port = process.env.PORT || "5000"; 
+  var url  = ['http://','0.0.0.0:',port].join("");
   
-zoster(port).listen(port, '0.0.0.0', function(err){
-  if(err) return quit(err);
-});
+  zoster(port).listen(port, '0.0.0.0', function(err){
+    if(err) return (cb !=undefined ? cb({err:err}) : console.log(err));
+    if(cb !=undefined) cb({url:url, err:false});
+  });
 
-var open = require('child_process').spawn;		
-open("open", [url]);
+  if(open != undefined && open) {
+    var open = require('child_process').spawn;
+    open("open", [url]);
+  }
+}
