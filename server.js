16d64ec7c32452c0ff22cd5c0e322f36fed6edae
vrunoa <vruno.alassia@gmail.com>
2015-06-25 12:53:13 -0300

    add cli support for zoster

--- server.js
+++ server.js
@@ -2,8 +2,7 @@
 module.exports = function(open, cb, caps) {
   var zoster = require('./node/');
   var port = process.env.PORT || "5000"; 
-  var url  = ['http://','0.0.0.0:',port].join("");
-  
+    
   zoster({port:port, caps:caps, cb:cb});
   
   if(open != undefined && open) {
