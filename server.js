6ced896c5a090310affee5a7cf41fd209359ba4e
vrunoa <vruno.alassia@gmail.com>
2015-06-26 00:14:43 -0300

    readable source code, prepared to add mocha tests

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ module.exports = function(open, cb, caps) {
   var zoster = require('./node/');
   var port = process.env.PORT || "5000"; 
     
-  zoster({port:port, caps:caps, cb:cb, open:open});
+  zoster({port:port, capabilities:caps, cb:cb, open:open}).run();
   
 }
