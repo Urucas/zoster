a0dcce95c5d2c3d2fd89b4d481270019d7129e8a
vrunoa <vruno.alassia@gmail.com>
2015-07-01 23:14:58 -0300

    fix execution paths

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = function(open, cb, caps) {
-  var zoster = require(process.cwd()+'/node/');
+  var zoster = require(__dirname+'/node/');
   var port = process.env.PORT || "5000"; 
     
   zoster({port:port, capabilities:caps, cb:cb, open:open}).run();
