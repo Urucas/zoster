a8a37704477fc08bf16a51e19cacc17e7f44aadd
vrunoa <vruno.alassia@gmail.com>
2017-01-02 15:01:37 -0300

    create symlinks

--- link.js
+++ link.js
@@ -4,6 +4,12 @@ var links = ['angular', 'bootstrap', 'jquery', 'socket.io', 'zeroclipboard']
 var path = require('path')
 for (let i in links) {
   let dist = path.join(__dirname, 'node', links[i])
+  try {
+    let stat = fs.statSync(dist)
+    if (stat) {
+      fs.unlinkSync(dist)
+    }
+  } catch (e) { }
   let libPath = path.join(__dirname, 'node_modules', links[i])
-  fs.symlinkSync(dist, libPath)
+  fs.symlinkSync(libPath, dist)
 }
