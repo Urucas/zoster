b89f08ae435bbc35822ad3d7be9ffeff2877a85c
vrunoa <vruno.alassia@gmail.com>
2017-01-03 10:18:22 -0300

    refactoring test to use async/await

--- link.js
+++ link.js
@@ -3,7 +3,7 @@ var fs = require('fs')
 var links = ['angular', 'bootstrap', 'jquery', 'socket.io', 'zeroclipboard']
 var path = require('path')
 for (let i in links) {
-  let dist = path.join(__dirname, 'node', links[i])
+  let dist = path.join(__dirname, 'public', links[i])
   try {
     let stat = fs.statSync(dist)
     if (stat) {
